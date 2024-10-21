ae62e833313cdfae94af4e0f9f161f8a04297f3d
Francisco Juarez <data.frankly@gmail.com>
2024-10-21 13:42:52 -0600

    updated main

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,17 +5,15 @@ import plotly.graph_objects as go
 # Load the data
 rp_grid_with_teams = pd.read_csv('rp_grid_active_pitchers_sorted.csv')
 
-# Select the last 6 columns for the dates (excluding 'team_name', 'player_name', and 'Total_Pitches')
-date_columns = rp_grid_with_teams.columns[2:-1]  # Assuming the last column is 'Total_Pitches'
+date_columns = rp_grid_with_teams.columns[2:-1]
 date_columns_reversed = date_columns[::-1]
 
-# Filter out players who haven't pitched in the last 6 days
 rp_grid_active_pitchers = rp_grid_with_teams.loc[
-    ~(rp_grid_with_teams[date_columns_reversed] == 0).all(axis=1),  # '~' is the negation operator
+    ~(rp_grid_with_teams[date_columns_reversed] == 0).all(axis=1),
     ['team_name', 'player_name'] + list(date_columns_reversed) + ['Lanzamientos Totales']
 ]
 
-# Reset the index for the clean DataFrame
+
 rp_grid_active_pitchers.reset_index(drop=True, inplace=True)
 
 # Define the team color map
@@ -32,56 +30,55 @@ team_color_map = {
     'GSV': '#85a8e2',
 }
 
-# Create a dropdown for team selection
+
 team_choice = st.selectbox('Selecciona un equipo:', rp_grid_active_pitchers['team_name'].unique())
 
-# Filter data for the selected team
 team_data = rp_grid_active_pitchers[rp_grid_active_pitchers['team_name'] == team_choice]
 
-# Remove columns (days) where all pitchers for the selected team have 0 values
+
 team_data = team_data.loc[:, (team_data != 0).any(axis=0)]
 
-# Extract date columns for dynamic plotting (after filtering out zero days)
-date_columns_team = team_data.columns[2:-1]  # Assuming the first two columns are team_name and player_name, last is Total_Pitches
 
-# Prepare the headers and cell values for the Plotly table (including Total_Pitches)
+date_columns_team = team_data.columns[2:-1] 
+
+
 headers = ['RP'] + [str(col) for col in date_columns_team] + ['Lanzamientos Totales']
 
-# Function to make non-zero values bold
+
 def format_cells_for_bold(team_data, columns):
     formatted_cells = []
     for col in columns:
         formatted_column = []
         for value in team_data[col]:
             if value != 0:
-                formatted_column.append(f"<b>{value}</b>")  # Make non-zero values bold
+                formatted_column.append(f"<b>{value}</b>") 
             else:
-                formatted_column.append(f"{value}")  # Keep zero values normal
+                formatted_column.append(f"{value}")  
         formatted_cells.append(formatted_column)
     return formatted_cells
 
-# Format the cells with bold for non-zero values (including Total Pitches)
+
 cells = [team_data['player_name']] + format_cells_for_bold(team_data, date_columns_team) + [team_data['Lanzamientos Totales']]
 
-# Get the team's color from the map
-team_color = team_color_map.get(team_choice, '#ffffff')  # Default to white if team not found
 
-# Create the table figure using Plotly
+team_color = team_color_map.get(team_choice, '#ffffff') 
+
+
 fig = go.Figure(data=[go.Table(
     header=dict(values=headers, fill_color=team_color, align='center',
                 font=dict(size=12, color='beige')),
     cells=dict(values=cells, fill_color='#a19174', align='center', format=['html'],
-               font=dict(size=12, family='Verdana', color='black'))  # Use 'html' to format bold text
+               font=dict(size=12, family='Verdana', color='black'))  
 )])
 
-# Add watermark annotation in the lower right corner
+# Add watermark 
 fig.add_annotation(
     go.layout.Annotation(
         text="@iamfrankjuarez",
-        x=0.999,  # Adjust X position for watermark (closer to 1 for the right edge)
-        y=1.006,  # Adjust Y position for watermark (closer to 0 for the bottom edge)
+        x=0.999,  
+        y=1.006, 
         showarrow=False,
-        font=dict(size=10, color='white'),  # Adjust font size and color
+        font=dict(size=10, color='white'),  
     )
 )
 
@@ -95,5 +92,4 @@ fig.update_layout(
     margin=dict(l=5, r=5, t=30, b=5)
 )
 
-# Display the figure in Streamlit
 st.plotly_chart(fig)
